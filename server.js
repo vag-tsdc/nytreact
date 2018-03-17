1a7a6f144feb25a62736bcfed8d39e41b3545bdf
vag-tsdc <vagtsdc@gmail.com>
2018-03-17 03:47:30 -0400

    push all edits saving this homework assignment

--- server.js
+++ server.js
@@ -3,21 +3,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 8080;
+const axios = require("axios");
+
+
+const port = process.env.PORT || 3001;
 
 // Import Routes + allow server access
 const routes = require("./routes");
 const app = express();
 
-// Content for the app pulled from PUBLIC in client/build
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
 app.use(express.static("client/build"));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+// Add routes, both API and view
+app.use(routes);
 
-// Routes
-app.use("/", routes);
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactNYTArticles",
+  {
+    useMongoClient: true
+  }
+);
 
-app.listen(port, function () {
-  console.log("listening on port", port);
+// document.onload {
+//   $('.tap-target').tapTarget('open');
+//   $('.tap-target').tapTarget('close');
+  
+// }
+
+
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
